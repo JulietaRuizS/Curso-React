63916b69bad264b792697454eb2c24c291d23e65
Julieta Ruiz Simancas <julietaruizsimancas@gmail.com>
2022-12-08 19:23:59 +0100

    Segunda entrega proyecto - Ruiz Simancas

---
+++ proyecto/src/components/ItemList.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ItemList = ({items}) => {
+    return(
+        <h1>ItemList</h1>
+    )
+}
+
+export default ItemList;
--- proyecto/src/components/ItemListContainer.jsx
+++ proyecto/src/components/ItemListContainer.jsx
@@ -1,15 +1,35 @@
 import React from "react";
+import arrayProductos from "./json/productos.json";
+import ItemCount from "./ItemCount";
+import { useEffect } from "react";
+import ItemList from "./ItemList";
 
-const ItemListContainer = () => {
+
+const ItemListContainer = ({greeting}) => {
+    const [items, setItems] = useState ([]);
+
+    useEffect(() => {
+        const promesa = new Promise ((resolve, reject) => {
+            setTimeout (() => { 
+                resolve (arrayProductos);
+            }, 2000);   
+        });
+        promesa.then((data)=> {
+            setItems(data); 
+        })
+    });
+
+
     return (
         <div className="container">
             <div className="row">
                 <div className="col-md-12">
                     <div className="alert alert-warning text-center" role= "alert">
-                        {greeting}
+                        <ItemList items ={items} />
                     </div>
                 </div>
             </div>
+            <ItemCount stock = {15}/>
         </div>
     )
 }
